a0198ccd95c1339c5d128187e156ed614b9228c9
christopher zinati <christopherzinati@gmail.com>
2024-04-25 12:13:13 -0700

    added dijkstra function (needs work)

--- NavigatorBackend.js
+++ NavigatorBackend.js
@@ -1,9 +1,16 @@
 const graph = {
-    A: { B: 1, C: 4 },      // Node A is connected to Node B with a weight of 1 and Node C with a weight of 4
-    B: { A: 1, C: 2, D: 5 },  // ... and so on for other nodes
-    C: { A: 4, B: 2, D: 1 },  // for testing purposes only
-    D: { B: 5, C: 1 }
+    'A': {'B': 5, 'C': 3},
+    'B': {'D': 2},
+    'C': {'D': 1},
+    'D': {}
 };
+
+const places = {
+    'A' : 'Titan Sudent Union',
+    'B' : 'State College Parking Structure',
+    'C' : 'Pollack Library',
+    'D' : 'Clayes Performing Arts Center'
+ }
 
 //for priority queue functionality
 class PriorityQueue {
@@ -28,3 +35,53 @@ class PriorityQueue {
         return this.queue.length === 0;
     }
 }
+
+function dijkstra(graph, origin, dest){
+    const distances = {};
+    const previous = {};
+    const pq = new PriorityQueue ();
+
+    for(let node in graph){
+        distances[node] = Infinity;
+        previous[node] = null;
+    }
+    distances[origin] = 0;
+    pq.enqueue(origin, 0);
+    while(!pq.isEmpty()){
+        const {node : curr, priority : currDistance} = pq.dequeue();
+        if (curr === dest) break;
+
+        for(let neighbor in graph[curr]){
+            const distance = currDistance + graph[curr][neighbor];
+            if(distance < distances[neighbor]){
+                distances[neighbor] = distance;
+                previous[neighbor] = curr;
+                pq.enqueue(neighbor, distance);
+            }
+        }
+    }
+
+    if (previous[dest] === null){
+        return {path: [], distance: Infinity};
+    }
+
+    const shortestPathNodes = [];
+    let node = dest;
+    while(node !== null){
+        shortestPathNodes.unshift(node);
+        node = previous[node];
+    }
+
+    const shortestPath = {
+        path : shortestPathNodes.map(node => places[node]),
+        distance : distances[dest]
+    };
+
+    return shortestPath;
+}
+const sourceNode = 'A';
+const destinationNode = 'C';
+
+const result = dijkstra(graph, places, sourceNode, destinationNode);
+console.log(`Shortest path from ${places[sourceNode]} to ${places[destinationNode]}:`, result.path);
+console.log(`Total distance: ${result.distance}`);
